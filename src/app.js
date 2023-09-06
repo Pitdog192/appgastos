6cb34952f85f018abbdd31ba77668e9651ef6dcb
santiago <arroqui192@gmail.com>
2023-09-06 13:36:19 -0300

    Crt

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import express from 'express'
+import https from 'https'
+import fs from 'fs'
 
 const app = express();
-
-const port = 3333
+const port = 443
+const httpsOptions = {
+    key: fs.readFileSync('../crt/arrsanger.com.ar.key'),      // Ruta a tu archivo de clave privada
+    cert: fs.readFileSync('../crt/arrsanger.com.ar.crt') // Ruta a tu archivo de certificado
+};
 
 app.get('/', (req, res) => {
-    res.send('<h1>Root</h1>')
+    res.send('<h1>Root asd</h1>')
+    console.log(httpsOptions)
 })
 
-app.listen(port, () => console.log(`Escuchando puerto ${port}`))
+const server = https.createServer(httpsOptions, app);
+server.listen(port, () => {
+    console.log('Servidor HTTPS activo en el puerto 443');
+});
