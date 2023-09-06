725f48266f69a25bb2da0a11a8d95bf5b1c0c118
santiago <arroqui192@gmail.com>
2023-09-06 14:18:31 -0300

    pruebas

--- src/app.js
+++ src/app.js
@@ -1,20 +1,12 @@
 import express from 'express'
-import https from 'https'
-import fs from 'fs'
 
 const app = express();
 const port = 3333
-const httpsOptions = {
-    key: fs.readFileSync('../crt/arrsanger.com.ar.key'),      // Ruta a tu archivo de clave privada
-    cert: fs.readFileSync('../crt/arrsanger.com.ar.crt') // Ruta a tu archivo de certificado
-};
 
 app.get('/root', (req, res) => {
     res.send('<h1>Root asd</h1>')
-    console.log(httpsOptions)
 })
 
-const server = https.createServer(httpsOptions, app);
-server.listen(port, () => {
-    console.log('Servidor HTTPS activo en el puerto 443');
+app.listen(port, () => {
+    console.log(`Servidor escuchando puerto : ${server.address().port}`)
 });
